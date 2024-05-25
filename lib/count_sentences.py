34d8d2236f814f68b717a99af4626b4bac53e646
Samuel <samuelkirori300@gmail.com>
2024-05-25 22:09:24 +0300

    Done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -23,16 +23,6 @@ class MyString:
         return len(sentences)
 
 
-# Test case
-try:
-    # Providing a non-string value
-    my_string = MyString(123)
-except ValueError as e:
-    print(e)  # Output: The value must be a string.
-
-# Providing a proper string value
-my_string = MyString("This is a test sentence. This is another test sentence.")
-print(my_string.count_sentences())  # Output: 2
 
 
 
